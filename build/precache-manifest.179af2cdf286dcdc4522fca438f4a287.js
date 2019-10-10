3e3288bbc1546025b3417f83d2f888d1929b083b
azucco <alessandro.zucco@gmail.com>
2019-10-10 22:28:33 +0200

    build commit

---
+++ build/precache-manifest.179af2cdf286dcdc4522fca438f4a287.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3fdb0fcd23406745bdf80be926ea944a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e0748ddba1dfb67c9d8e",
+    "url": "/static/css/2.510409cd.chunk.css"
+  },
+  {
+    "revision": "22d826fb8ce26b217219",
+    "url": "/static/css/main.30534edc.chunk.css"
+  },
+  {
+    "revision": "e0748ddba1dfb67c9d8e",
+    "url": "/static/js/2.11c0414d.chunk.js"
+  },
+  {
+    "revision": "22d826fb8ce26b217219",
+    "url": "/static/js/main.a70f6e1f.chunk.js"
+  },
+  {
+    "revision": "5d89a04b85ca938887d4",
+    "url": "/static/js/runtime-main.262b547a.js"
+  },
+  {
+    "revision": "b8357b05fe711a44aa29e82404d83325",
+    "url": "/static/media/ecm_logo.b8357b05.png"
+  },
+  {
+    "revision": "c6113febc50e12342049df29a2b991df",
+    "url": "/static/media/ecm_logo_darkbg.c6113feb.png"
+  }
+]);
